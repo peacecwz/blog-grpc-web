c34e05714e1ade07e3fe44607532228434c59066
Baris Ceviz <me@barisceviz.com>
2020-08-03 12:45:23 +0300

    Init grpc-web project

---
+++ src/Blog.Client/Messages/WeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Blog.Contract
+{
+    internal partial class WeatherForecast
+    {
+        public DateTime DateTime => Date.ToDateTime();
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
