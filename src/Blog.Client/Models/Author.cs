0aff71847a8621cfbc6e589495d935ef79d8279c
Baris Ceviz <me@barisceviz.com>
2020-08-05 09:31:31 +0300

    Refactor project

---
+++ src/Blog.Client/Models/Author.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Blog.Client.Models
+{
+    public class Author
+    {
+        [JsonPropertyName("fullName")]
+        public string FullName { get; set; }
+        
+        [JsonPropertyName("biography")]
+        public string Biography { get; set; }
+        
+        [JsonPropertyName("profileImage")]
+        public string ProfileImage { get; set; }
+
+        [JsonPropertyName("socialLinks")]
+        public Dictionary<string,string> SocialLinks { get; set; }
+    }
+}
