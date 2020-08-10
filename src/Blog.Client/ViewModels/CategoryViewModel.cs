980a02866637394f62ed4e9aee3235f4ad65cfb9
Baris Ceviz <me@barisceviz.com>
2020-08-10 03:53:04 +0300

    Add Category view model

---
+++ src/Blog.Client/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.Client.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
